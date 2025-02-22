c7a9f78824ea12f74fe8904e12f153fa861e81a2
abo3adel <abo3adel35@gmail.com>
2025-02-22 23:42:40 +0200

    guests

--- Modules/Guests/app/Models/Guest.php
+++ Modules/Guests/app/Models/Guest.php
@@ -6,12 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Auth\Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 use Modules\Guests\Database\Factories\GuestFactory;
 
 class Guest
 {
     /** @use HasFactory<\Modules\Guests\Database\Factories\GuestFactory> */
-    use HasFactory, Authenticatable, Authorizable;
+    use HasFactory, Authenticatable, Authorizable, HasApiTokens;
 
 
     /**
