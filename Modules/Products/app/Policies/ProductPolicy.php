1d13b5364e7e86d6b60a4e79357494f7085fb41f
abo3adel <abo3adel35@gmail.com>
2025-03-12 22:36:13 +0200

    feat(products): add policies

---
+++ Modules/Products/app/Policies/ProductPolicy.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Modules\Products\Policies;
+
+use Modules\Users\Models\User;
+use Modules\Products\Models\Product;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProductPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Product $product): bool
+    {
+        return $user->can("view_product");
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can("create_product");
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Product $product): bool
+    {
+        return $user->can("update_product");
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Product $product): bool
+    {
+        return $user->can("delete_product");
+    }
+}
