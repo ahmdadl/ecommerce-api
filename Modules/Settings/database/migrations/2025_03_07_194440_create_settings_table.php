a3b7656ea8252d146ed41429b5934a9fbe947869
abo3adel <abo3adel35@gmail.com>
2025-05-10 17:50:02 +0300

    feat: deploy

--- Modules/Settings/database/migrations/2025_03_07_194440_create_settings_table.php
+++ Modules/Settings/database/migrations/2025_03_07_194440_create_settings_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
     {
         Schema::create("settings", function (Blueprint $table) {
             $table->id();
-            $table->json("data")->index()->nullable();
+            $table->json("data")->nullable();
             $table->timestamps();
         });
 
