c8bac87a0d2f5e08c7d66236fd34e90f633e92e9
abo3adel <abo3adel35@gmail.com>
2025-05-10 04:05:17 +0300

    feat(wallets): add trans

---
+++ Modules/Wallets/lang/ar/t.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    "user_not_logged_in" => "المستخدم غير مسجل الدخول",
+    "amount_must_be_positive" => "يجب أن يكون المبلغ موجباً",
+    "amount_must_be_gte" => "يجب أن يكون المبلغ أكبر من أو يساوي :other_name",
+    "amount_must_be_lte" => "يجب أن يكون المبلغ أقل من أو يساوي :other_name",
+    "wallet_not_found" => "المحفظة غير موجودة",
+    "wallet_not_active" => "المحفظة غير مفعلة",
+    "wallet_not_enough_balance" => "لا يوجد رصيد كافٍ في المحفظة",
+    "pending_amount" => "المبلغ المعلق",
+    "available_amount" => "المبلغ المتاح",
+    "total_amount" => "المبلغ الإجمالي",
+    "types" => [
+        "credit" => "ائتمان",
+        "debit" => "مدين",
+    ],
+    "transaction_statuses" => [
+        "pending" => "قيد الانتظار",
+        "completed" => "مكتمل",
+        "canceled" => "ملغى",
+    ],
+];
