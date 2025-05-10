f19750e4a6bbf40fe7c464e1aecf67a2466cf45f
abo3adel <abo3adel35@gmail.com>
2025-05-11 00:26:48 +0300

    feat: deploy

--- Modules/Orders/resources/views/filament/resources/order-resource/pages/order-details-page.blade.php
+++ Modules/Orders/resources/views/filament/resources/order-resource/pages/order-details-page.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
     <div class="space-y-6">
-        {{-- @vite('resources/css/filament/admin/theme.css') --}}
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app/theme.css') }}" />
 
         <x-orders::order-summary :order="$order" :orderStatus="$orderStatus" :paymentStatus="$paymentStatus" />
 
