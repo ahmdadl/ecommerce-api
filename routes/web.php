2f13170671aa25074fcf9990756792770104c248
abo3adel <abo3adel35@gmail.com>
2025-06-16 02:09:47 +0300

    feat: add real products iamges

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\Products\Database\Seeders\ProductsDatabaseSeeder;
 
 Route::get("/", function () {
-    (new ProductsDatabaseSeeder())->run();
-
     return [
         "Laravel" => app()->version(),
         // "settings" => new SettingResource([]),
